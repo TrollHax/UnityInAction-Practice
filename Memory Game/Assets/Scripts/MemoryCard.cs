e16d07696babd0c7fe45d0bb0f2f005242755e37
Reymond T <91326933+TrollHax@users.noreply.github.com>
2023-02-02 10:46:30 +0100

    Added Score, Checking for Pairs

--- Memory Game/Assets/Scripts/MemoryCard.cs
+++ Memory Game/Assets/Scripts/MemoryCard.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class MemoryCard : MonoBehaviour
 {
     [SerializeField] private GameObject cardBack;
+    [SerializeField] private SceneController controller;
+
+    // Getter function
+    private int _id;
+    public int id
+    {
+        get { return _id; }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -20,9 +28,22 @@ public class MemoryCard : MonoBehaviour
 
     public void OnMouseDown()
     {
-        if (cardBack.activeSelf)
+        if (cardBack.activeSelf && controller.canReveal)
         {
             cardBack.SetActive(false);
+            controller.CardRevealed(this);
         }
+    }
+
+    public void Unreveal()
+    {
+        cardBack.SetActive(true);
+    }
+
+    // Method that allows other scripts to pass new sprites to this object
+    public void SetCard(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
     }
 }
