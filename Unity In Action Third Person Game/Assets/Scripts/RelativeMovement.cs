2bc06656a84f4c8f05617ebdb0d34f7bad3a3606
Reymond T <91326933+TrollHax@users.noreply.github.com>
2023-03-06 10:33:55 +0100

    Added movement to character

--- Unity In Action Third Person Game/Assets/Scripts/RelativeMovement.cs
+++ Unity In Action Third Person Game/Assets/Scripts/RelativeMovement.cs
@@ -2,14 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
+
 public class RelativeMovement : MonoBehaviour
 {
     [SerializeField] Transform target;
 
+    public float rotSpeed = 15.0f;
+    public float moveSpeed = 6.0f;
+    public float jumpSpeed = 15.0f;
+    public float gravity = -9.8f;
+    public float terminalVelocity = -10.0f;
+    public float minFall = -1.5f;
+
+    private float vertSpeed;
+    private CharacterController charController;
+    private ControllColliderHit contact;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        charController = GetComponent<CharacterController>();
+        vertSpeed = minFall;
     }
 
     // Update is called once per frame
@@ -24,8 +38,55 @@ public class RelativeMovement : MonoBehaviour
             Vector3 right = target.right;
             Vector3 forward = Vector3.Cross(right, Vector3.up);
             movement = (right * horInput) + (forward * vertInput);
+            movement *= moveSpeed;
+            movement = Vector3.ClampMagnitude(movement, moveSpeed);
 
-            transform.rotation = Quaternion.LookRotation(movement);
+            Quaternion direction = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotSpeed * Time.deltaTime);
         }
+
+        bool hitGround = false;
+        RaycastHit hit;
+        if (vertSpeed < 0 &&
+            Physics.Raycast(transform.position, Vector3.down, out hit))
+        {
+            float check = (charController.height + charController.radius) / 1.9f;
+            hitGround = hit.distance <= check;
+        }
+
+        if (hitGround)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                vertSpeed = jumpSpeed;
+            }
+            else
+            {
+                vertSpeed = minFall;
+            }
+        }
+        else
+        {
+            vertSpeed += gravity * 5 * Time.deltaTime;
+            if (vertSpeed < terminalVelocity)
+            {
+                vertSpeed = terminalVelocity;
+            }
+        }
+        if (charController.isGrounded)
+        {
+            if (Vector3.Dot(movement, contact.normal < 0)
+            {
+                movement = contact.normal * moveSpeed;
+            }
+            else
+            {
+                movement += contact.normal * moveSpeed;
+            }
+        }
+        movement.y = vertSpeed;
+
+        movement *= Time.deltaTime;
+        charController.Move(movement);
     }
 }
