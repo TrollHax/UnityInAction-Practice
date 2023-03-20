52fc770e64c21bc2893cfdb6486d2c3d4ea41f86
Reymond T <91326933+TrollHax@users.noreply.github.com>
2023-03-20 10:28:37 +0100

    Added collectibles

--- Unity In Action Third Person Game+Interactive Devices/Assets/Scripts/OrbitCamera.cs
+++ Unity In Action Third Person Game+Interactive Devices/Assets/Scripts/OrbitCamera.cs
@@ -7,18 +7,16 @@ public class OrbitCamera : MonoBehaviour
     [SerializeField] Transform target;
 
     public float rotSpeed = 1.5f;
+    public float sensitivity = 3f;
 
     private float rotY;
     private Vector3 offset;
-    private Vector3 ogOffset;
-    private RaycastHit hit;
 
     // Start is called before the first frame update
     void Start()
     {
         rotY = transform.eulerAngles.y;
-        ogOffset = target.position - transform.position;
-        offset = ogOffset;
+        offset = target.position - transform.position;
     }
 
     // Update is called once per frame
@@ -29,15 +27,10 @@ public class OrbitCamera : MonoBehaviour
 
     void LateUpdate()
     {
-        rotY += Input.GetAxis("Mouse X") * rotSpeed;
+        rotY += Input.GetAxis("Mouse X") * rotSpeed * sensitivity;
 
         Quaternion rotation = Quaternion.Euler(0, rotY, 0);
         transform.position = target.position - (rotation * offset);
         transform.LookAt(target);
-
-        if (Physics.SphereCast(transform.position, 0.5f, target.position, out hit, 0.5f))
-        {
-            offset = new Vector3(Vector3.distance)
-        }
     }
 }
