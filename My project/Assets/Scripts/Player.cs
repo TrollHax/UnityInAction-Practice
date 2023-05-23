01be50b0b362f1fb0d7ff22f9f2070a5b59972f0
Reymond T <4433.RT@gmail.com>
2023-05-23 16:01:33 +0200

    Added world, animation to character

--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -8,10 +8,16 @@ public class Player : MonoBehaviour
     public LayerMask clickable;
 
     private NavMeshAgent myAgent;
+    private Animator animator;
+    private Rigidbody rb;
+    private Vector3 prevPos;
+    private float speed;
 
     void Start()
     {
         myAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -27,5 +33,10 @@ public class Player : MonoBehaviour
                 return;
             }
         }
+        speed = ((transform.position - prevPos).magnitude) / Time.deltaTime;
+        prevPos = transform.position;
+
+        animator.SetFloat("speed", speed);
+        Debug.Log(speed);
     }
 }
